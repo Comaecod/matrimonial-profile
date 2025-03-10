36a8e404369f29b4d90898d5dda789efefca923b
comaecod <vishnuthecoder@gmail.com>
2025-03-10 17:19:22 +0530

    add images + refactoring

---
+++ constants.js
@@ -0,0 +1,113 @@
+// Header Content
+const headerContent = {
+  title: "Chi. Venkata Vishnu's Matrimonial Profile",
+  subtitle: 'Details given by his mother, Smt. Kalyani.',
+};
+
+// Sections Content (Plain Array of Strings)
+const sectionsContent = [
+  `
+    Namaskaram dear parents, would-be brides, and grooms, I am Smt. Kalyani. This is my son's matrimonial profile. I want to share with you a few thoughts that have been close to my heart since I was young. I believe that marriage is a sacred ceremony that increases the joy manifold. The bride, in particular, should be given the utmost care and importance because she leaves her family to join a new one. She is, after all, referred to as <em>'Kanya Ratnam'</em> (her counterpart <em>'Vara Ratnam'</em>).
+  `,
+  `
+    When marriage is such a beautiful and romantic journey, why should girls and boys compromise or make haste? Shouldn't we take time to ensure compatibility in all aspects of life? For example, in my son's case, he would first prefer a bride who has <em>Daiva Bhakti</em>, <em>Guru Bhakti</em>, and dedication to family, as he himself embodies these qualities.
+    <p>Shouldn't these qualities be the top priorities for anyone?</p>
+  `,
+  `
+    For my son, a girl interested in learning subjects such as Computers, English Literature, Cosmology, Sanskrit, Music (vocal or instrumental), Kuchipudi, or Bharatanatyam would be greatly preferred. My son has a keen interest and some knowledge in these areas, and he would love to appreciate, enjoy, share and grow in these fields with his life partner.
+  `,
+  `
+    Don't you agree with me when I say that the bride and groom should find each other physically attractive and have meaningful conversations before stepping into <em>Grihastashrama</em>?
+    <p>Are not compatibility and mutual understanding the foundations of a happy and lasting marriage?</p>
+  `,
+  `
+    If you agree with my thoughts, kindly contact me if you have such a girl in your family or if you would like to share your views. I look forward to hearing from you.
+  `,
+];
+
+// Profile Details
+const profileDetails = {
+  title: 'Profile Details',
+  data: [
+    { category: 'Subsect', details: 'Vaidiki Velanadu' },
+    { category: 'Gothram', details: 'Koundinyasa' },
+    { category: 'Surname', details: 'Parasuramuni' },
+    { category: 'Name', details: 'Venkata Vishnu' },
+    { category: 'Date of Birth', details: '2nd October, 1998' },
+    { category: 'Time of Birth', details: '09:53 a.m.' },
+    { category: 'Place of Birth', details: 'Guntakal' },
+    { category: 'Birth Star', details: 'Dhanishta' },
+    { category: 'Raasi', details: 'Makara' },
+    { category: 'Height', details: '6 feet' },
+    { category: 'Complexion', details: 'Wheatish' },
+    { category: 'Siblings', details: 'None' },
+    {
+      category: 'Father',
+      details:
+        'Sri. Sairam Parasuramuni (Deputy Commissioner, Cabinet Secretariat)',
+    },
+    {
+      category: 'Mother',
+      details:
+        'Smt. Kalyani Parasuramuni (MA English, SK University, Homemaker)',
+    },
+    { category: 'Type', details: 'Smartha' },
+    { category: 'Matha', details: 'Sringeri' },
+    { category: 'Native Place', details: 'Hyderabad' },
+    { category: 'Diet', details: 'Vegetarian' },
+    {
+      category: 'Education',
+      details: 'Master of Computer Applications, Sastra University',
+    },
+    {
+      category: 'Work',
+      details: `
+        <ul>
+          <li>Joining as a Computer Teacher at Shankara School (Sampradaya Pathashala), CBSE, Kanchi Peetham, Podili with 3.24 LPA.</li>
+          <li>Worked as Software Engineer at Zenoti, Hyderabad with 13.7 LPA.</li>
+        </ul>
+      `,
+    },
+  ],
+};
+
+// Image Gallery
+const imageGallery = {
+  title: 'Photos',
+  images: [
+    'meInSringeriSidePhoto.jpg',
+    'meNearBushes.jpg',
+    'meOnAHammock.jpg',
+    'meAtThePool.jpg',
+    'meSittingInSringeri.jpg',
+    'meWithAGuitar.jpg',
+  ],
+};
+
+// Contact Details
+const contactDetails = {
+  title: 'Contact Details',
+  content: `
+    Contact Number: +91 9321232599
+    <img src="assets/icons/NoWhatsApp.png" alt="No WhatsApp" class="whatsapp-icon" loading="lazy" fetchpriority="low"/>
+  `,
+};
+
+// Requirements
+const requirements = {
+  title: 'Requirements',
+  items: [
+    'Traditional family background.',
+    'Preferred age gap: 8 years to minimum 3 years.',
+    'Looking for a girl who can happily be a homemaker.',
+  ],
+};
+
+// Footer Content
+const footerContent = {
+  content: `
+    For more details like hobbies, job experience, education,
+    testimonials, and social media handles ,checkout
+    <a href="https://comaecod.github.io/portfolio/" target="_blank">Vishnu's website</a>.
+  `,
+};
---
+++ script.js
@@ -0,0 +1,108 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+  // Append header content
+  header.innerHTML = `
+    <h3>ॐ</h3>
+    <h1>${headerContent.title}</h1>
+    <p><i>${headerContent.subtitle}</i></p>
+  `;
+
+  // Append sections
+  sectionsContent.forEach((section) => {
+    const sectionElement = document.createElement('section');
+    sectionElement.innerHTML = `<p>${section}</p>`;
+    main.appendChild(sectionElement);
+  });
+
+  // Append profile details table
+  const profileSection = document.createElement('section');
+  profileSection.innerHTML = `<h2>${profileDetails.title}</h2>`;
+  const table = document.createElement('table');
+  table.innerHTML = `
+    <tr>
+      <th>Category</th>
+      <th>Details</th>
+    </tr>
+    ${profileDetails.data
+      .map(
+        (row) => `
+      <tr>
+        <td>${row.category}</td>
+        <td>${row.details}</td>
+      </tr>
+    `
+      )
+      .join('')}
+  `;
+  profileSection.appendChild(table);
+  main.appendChild(profileSection);
+
+  // Append image gallery
+  const imageGallerySection = document.createElement('section');
+  imageGallerySection.classList.add('image-gallery');
+  imageGallerySection.innerHTML = `<h2>${imageGallery.title}</h2>`;
+  const gallery = document.createElement('div');
+  gallery.classList.add('gallery');
+
+  // Create modal for mobile view
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.innerHTML = `
+    <span class="close-modal">&times;</span>
+    <img class="modal-content" id="modal-image" />
+  `;
+  document.body.appendChild(modal);
+
+  // Add event listeners to images for mobile view
+  imageGallery.images.forEach((image) => {
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+    imageContainer.innerHTML = `<img src="assets/photos/${image}" alt="Gallery Image" loading="lazy" fetchpriority="low"/>`;
+    gallery.appendChild(imageContainer);
+    imageContainer.querySelector('img').addEventListener('click', () => {
+      modal.style.display = 'block';
+      modal.querySelector('#modal-image').src = 'assets/photos/' + image;
+    });
+  });
+
+  imageGallerySection.appendChild(gallery);
+  main.appendChild(imageGallerySection);
+
+  // Close modal when clicking on the close button
+  modal.querySelector('.close-modal').addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  // Close modal when clicking outside the image
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+
+  // Append contact details
+  const contactSection = document.createElement('section');
+  contactSection.classList.add('contact-details');
+  contactSection.innerHTML = `
+    <h2>${contactDetails.title}</h2>
+    <p>${contactDetails.content}</p>
+  `;
+  main.appendChild(contactSection);
+
+  // Append requirements
+  const requirementsSection = document.createElement('section');
+  requirementsSection.innerHTML = `<h2>${requirements.title}</h2>`;
+  const list = document.createElement('ul');
+  list.classList.add('requirements');
+  requirements.items.forEach((item) => {
+    list.innerHTML += `<li>${item}</li>`;
+  });
+  requirementsSection.appendChild(list);
+  main.appendChild(requirementsSection);
+
+  // Append footer content
+  footer.innerHTML = `<p>${footerContent.content}</p>`;
+});
